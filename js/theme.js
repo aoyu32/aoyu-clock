a8658859f6242563f662b73b75ebfe16e03d7192
aoyu <aoyu3399@gmail.com>
2024-09-20 17:01:32 +0800

    修复切换深浅模式时时钟过渡不自然问题

--- js/theme.js
+++ js/theme.js
@@ -1,10 +1,10 @@
-const cards = document.getElementsByClassName("card-box")
 const root = document.documentElement
+const card2 = document.querySelector(".card2")
+const card3 = document.querySelector(".card3")
+window.api.onMainMsg('set-win-theme', (event, data) => {
 
-window.api.onMainMsg('set-win-theme', (event,data) => {
+    console.log("main send msg:", data);
 
-    console.log("main send msg:",data);
-    
     changeBoxColor(data)
 
 
@@ -15,15 +15,24 @@ function changeBoxColor(model) {
     switch (model) {
         case "dark":
             console.log("is dark");
+            root.style.setProperty('--cards-transition', '0s')
             root.style.setProperty('--card-color', '#2e2e2e')
-            root.style.setProperty('--card-number-color','#e3e8ee')
-            root.style.setProperty('--colon-color','#e3e8ee')
+            root.style.setProperty('--card-number-color', '#e3e8ee')
+            root.style.setProperty('--colon-color', '#e3e8ee')
+            setTimeout(() => {
+                root.style.setProperty('--cards-transition', '1s')
+            }, 50)
+            // root.style.setProperty('--cards-transition','1s')
             break;
         case "light":
             console.log("is light");
+            root.style.setProperty('--cards-transition', '0s')
             root.style.setProperty('--card-color', '#e3e8ee')
-            root.style.setProperty('--card-number-color','#2e2e2e')
-            root.style.setProperty('--colon-color','#2e2e2e')
+            root.style.setProperty('--card-number-color', '#2e2e2e')
+            root.style.setProperty('--colon-color', '#2e2e2e')
+            setTimeout(() => {
+                root.style.setProperty('--cards-transition', '1s')
+            }, 50)
             break;
     }
 
